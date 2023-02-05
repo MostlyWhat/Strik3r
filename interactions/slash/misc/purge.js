4ade3aafb537e2b31301f1678e0964317728e7da
MostlyWhat <mostlywhatstudios@gmail.com>
2023-02-05 21:44:10 +0700

    Updated Purge Command

--- interactions/slash/misc/purge.js
+++ interactions/slash/misc/purge.js
@@ -7,7 +7,15 @@
 
 // Deconstructed the constants we need in this file.
 
-const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
+const {
+	SlashCommandBuilder,
+	PermissionFlagsBits,
+	ActionRowBuilder,
+	Events,
+	ModalBuilder,
+	TextInputBuilder,
+	TextInputStyle,
+} = require('discord.js');
 
 /**
  * @type {import('../../../typings').SlashInteractionCommand}
@@ -21,7 +29,44 @@ module.exports = {
 		.setDefaultMemberPermissions(PermissionFlagsBits.ManageMessages),
 
 	async execute(interaction) {
-		// send hello world to Discord
-		await interaction.reply('Hello World!');
+		if (!interaction.isChatInputCommand()) return;
+
+		if (interaction.commandName === 'purge') {
+			// Create the modal
+			const modal = new ModalBuilder()
+				.setCustomId('myModal')
+				.setTitle('My Modal');
+
+			// Add components to modal
+
+			// Create the text input components
+			const favoriteColorInput = new TextInputBuilder()
+				.setCustomId('favoriteColorInput')
+				// The label is the prompt the user sees for this input
+				.setLabel('What\'s your favorite color?')
+				// Short means only a single line of text
+				.setStyle(TextInputStyle.Short);
+
+			const hobbiesInput = new TextInputBuilder()
+				.setCustomId('hobbiesInput')
+				.setLabel('What\'s some of your favorite hobbies?')
+				// Paragraph means multiple lines of text.
+				.setStyle(TextInputStyle.Paragraph);
+
+			// An action row only holds one text input,
+			// so you need one action row per text input.
+			const firstActionRow = new ActionRowBuilder().addComponents(
+				favoriteColorInput,
+			);
+			const secondActionRow = new ActionRowBuilder().addComponents(
+				hobbiesInput,
+			);
+
+			// Add inputs to the modal
+			modal.addComponents(firstActionRow, secondActionRow);
+
+			// Show the modal to the user
+			await interaction.showModal(modal);
+		}
 	},
 };
